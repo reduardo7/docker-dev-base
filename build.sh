d3e29ea7901c88040aa2ce85b1b5430ad7b0c8c5
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-09 09:31:40 -0300

    Build & Install

--- build.sh
+++ build.sh
@@ -16,31 +16,6 @@ e() {
     echo "# $@"
 }
 
-# Install
-
-if ! type curl > /dev/null
-    then
-        s ; e "Installing wget..."
-        sudo sudo apt-get install -f curl
-    fi
-
-if ! type docker > /dev/null
-    then
-        s ; e "Installing docker..."
-        #wget -qO- https://get.docker.com/ | sh
-        curl -sSL https://get.docker.com/ | sh
-        s ; e "Adding $USER to docker group..."
-        sudo usermod -aG docker $USER
-        e "Remember that you will have to log out and back in for this to take effect!"
-        e
-        e
-        e "Testing..."
-        if ! sudo docker run hello-world
-            then
-                e "Error!"
-            fi
-    fi
-
 # Build
 
 image_name="$1"
