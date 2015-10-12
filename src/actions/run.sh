0f7794c14da58d35215577c6bb57d0476c5d4e0e
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-12 20:03:10 -0300

    Container Name

--- src/actions/run.sh
+++ src/actions/run.sh
@@ -1,6 +1,9 @@
-##
+## [name]
 ## Run container
-## Example: docker run -p <host_port1>:<container_port1> -p <host_port2>:<container_port2> -i -t DOCKDEV_IMAGE /bin/bash
+## Params:
+##     name: Container Name. Default: "default"
+## Example:
+##     docker run -p <host_port1>:<container_port1> -p <host_port2>:<container_port2> -i -t DOCKDEV_IMAGE /bin/bash
 
 # docker images
 # docker ps
@@ -11,7 +14,12 @@ e "Command: $(style bold)${DOCKDEV_CMD}"
 . $SOURCES_PATH/hooks.sh
 
 local cmd="docker run"
+local name="$1"
 local port
+
+if [ -z "$name" ]; then
+  name="$DOCKDEV_CONTAINER_NAME_DEFAULT"
+fi
 
 # Ports
 if [ ! -z "${DOCKDEV_PORTS}" ]; then
@@ -27,6 +35,8 @@ if [ ! -z "${DOCKDEV_MOUNTS}" ]; then
   done
 fi
 
-before_run
 set_on_exit after_run
-$cmd -i -t ${DOCKDEV_IMAGE} ${DOCKDEV_CMD}
+before_run
+
+e Running $(style bold)${name}$(style normal) [$(style bold)${DOCKDEV_IMAGE}$(style normal)]
+$cmd -name ${name} -i -t ${DOCKDEV_IMAGE} ${DOCKDEV_CMD}
