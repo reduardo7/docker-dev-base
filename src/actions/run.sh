df2ad742375e40d4e4eb291dfee4702249fe8d8e
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-12 19:42:33 -0300

    Services

--- src/actions/run.sh
+++ src/actions/run.sh
@@ -8,14 +8,25 @@
 e "Running $(style bold)${DOCKDEV_IMAGE}"
 e "Command: $(style bold)${DOCKDEV_CMD}"
 
+. $SOURCES_PATH/hooks.sh
+
 local cmd="docker run"
 local port
 
+# Ports
 if [ ! -z "${DOCKDEV_PORTS}" ]; then
-  # Without ports
   for port in ${DOCKDEV_PORTS[@]}; do
     cmd="${cmd} -p ${port}"
   done
 fi
 
+# Mounts
+if [ ! -z "${DOCKDEV_MOUNTS}" ]; then
+  for mnt in ${DOCKDEV_MOUNTS[@]}; do
+    cmd="${cmd} -v ${mnt}"
+  done
+fi
+
+before_run
+set_on_exit after_run
 $cmd -i -t ${DOCKDEV_IMAGE} ${DOCKDEV_CMD}
---
+++ src/sources/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+service apache2 start
+bash
+run chmod a+x /home/init.sh
