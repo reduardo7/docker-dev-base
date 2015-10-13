b7d6aecbea0cbfbef9fc1488f357d6c919900276
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-12 23:28:06 -0300

    Container start

--- src/actions/run.sh
+++ src/actions/run.sh
@@ -8,46 +8,42 @@
 # docker images
 # docker ps
 
-e "Running $(style bold)${DOCKDEV_IMAGE}"
+e "Image: $(style bold)${DOCKDEV_IMAGE}"
 e "Command: $(style bold)${DOCKDEV_CMD}"
 
 . $SOURCES_PATH/hooks.sh
 
 local cmd="docker run"
 local name="$1"
-local port
 
 if [ -z "$name" ]; then
   name="$DOCKDEV_CONTAINER_NAME_DEFAULT"
 fi
 
-# Ports
-if [ ! -z "${DOCKDEV_PORTS}" ]; then
-  for port in ${DOCKDEV_PORTS[@]}; do
-    cmd="${cmd} -p ${port}"
-  done
-fi
-
-# Mounts
-if [ ! -z "${DOCKDEV_MOUNTS}" ]; then
-  for mnt in ${DOCKDEV_MOUNTS[@]}; do
-    cmd="${cmd} -v ${mnt}"
-  done
-fi
-
-# Name Action
-local name_action
-if docker ps -a | egrep "\s${name}$"
-  then
-    name_action="link"
-  else
-    name_action="name"
-  fi
+local name_action="${DOCKDEV_IMAGE}.${name}"
 
 # Prepare
 set_on_exit after_run
 before_run
 
 # Run
-e "Running $(style bold)${name}$(style normal) [$(style bold)${name_action} -> ${DOCKDEV_IMAGE}$(style normal)]"
-$cmd --${name_action}=${name} -i -t ${DOCKDEV_IMAGE} ${DOCKDEV_CMD}
+e "Running $(style bold)${name}$(style normal) [$(style bold)${name_action}$(style normal)]"
+if docker ps -a | egrep "\s${name_action}$" > /dev/null
+  then
+    docker start -i ${name_action}
+  else
+    # Ports
+    if [ ! -z "${DOCKDEV_PORTS}" ]; then
+      for port in ${DOCKDEV_PORTS[@]}; do
+        cmd="${cmd} -p ${port}"
+      done
+    fi
+    # Mounts
+    if [ ! -z "${DOCKDEV_MOUNTS}" ]; then
+      for mnt in ${DOCKDEV_MOUNTS[@]}; do
+        cmd="${cmd} -v ${mnt}"
+      done
+    fi
+    # Run
+    $cmd --name=${name_action} -i -t ${DOCKDEV_IMAGE} ${DOCKDEV_CMD}
+  fi
