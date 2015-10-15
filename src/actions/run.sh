e4801475ac5293475f435f40319fe9052812a42e
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-15 12:17:02 -0300

    Run check Image exists

--- src/actions/run.sh
+++ src/actions/run.sh
@@ -23,27 +23,38 @@ fi
 local name_action="${DOCKDEV_IMAGE}.${name}"
 
 # Prepare
-set_on_exit after_run
-before_run
+_prepare() {
+  set_on_exit after_run
+  before_run
+}
 
 # Run
 e "Running $(style bold)${name}$(style normal) [$(style bold)${name_action}$(style normal)]"
 if docker ps -a | egrep "\s${name_action}$" > /dev/null
   then
+    # Start container
+    _prepare
     docker start -i ${name_action}
   else
-    # Ports
-    if [ ! -z "${DOCKDEV_PORTS}" ]; then
-      for port in ${DOCKDEV_PORTS[@]}; do
-        cmd="${cmd} -p ${port}"
-      done
-    fi
-    # Mounts
-    if [ ! -z "${DOCKDEV_MOUNTS}" ]; then
-      for mnt in ${DOCKDEV_MOUNTS[@]}; do
-        cmd="${cmd} -v ${mnt}"
-      done
-    fi
-    # Run
-    $cmd --name=${name_action} -i -t ${DOCKDEV_IMAGE} #${DOCKDEV_CMD}
+    # Create container
+    if docker images | egrep "^${DOCKDEV_IMAGE}\s" > /dev/null
+      then
+        # Ports
+        if [ ! -z "${DOCKDEV_PORTS}" ]; then
+          for port in ${DOCKDEV_PORTS[@]}; do
+            cmd="${cmd} -p ${port}"
+          done
+        fi
+        # Mounts
+        if [ ! -z "${DOCKDEV_MOUNTS}" ]; then
+          for mnt in ${DOCKDEV_MOUNTS[@]}; do
+            cmd="${cmd} -v ${mnt}"
+          done
+        fi
+        # Run
+        _prepare
+        $cmd --name=${name_action} -i -t ${DOCKDEV_IMAGE} #${DOCKDEV_CMD}
+      else
+        error "Image $(style bold)${DOCKDEV_IMAGE}$(style normal) not found! (run 'bash $0 build'?)"
+      fi
   fi
