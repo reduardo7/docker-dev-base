696a0ec9f5a389114cfc64e265403383b015527c
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-16 15:32:28 -0300

    Fixes bash

--- src/sources/docker-files/init.sh
+++ src/sources/docker-files/init.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # Executed every run
 
+if [ ! -d "~/.ssh" ]; then
+  if [ -d /root/home/.ssh ]; then
+    cp -r /root/home/.ssh ~/.ssh
+    chown -R $USERNAME:$USERNAME ~/.ssh
+    chmod -R 400 ~/.ssh
+  fi
+fi
+
+if [ ! -d "~/.git" ]; then
+  [ -d "/root/home/.git" ] && cp -r /root/home/.git ~/.git
+fi
+
 service mysql start
 service apache2 start
 
