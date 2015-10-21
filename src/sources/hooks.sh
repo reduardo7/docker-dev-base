11559a01b0cd5853dfd847ccde22dbd93dc67de6
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-21 09:21:53 -0300

    Fix Permissions

--- src/sources/hooks.sh
+++ src/sources/hooks.sh
@@ -1,11 +1,18 @@
+_fix_perm() {
+  e "Fix permissions $DOCKDEV_PROJECTS/*"
+  sudo chmod a+w $DOCKDEV_PROJECTS/*
+}
+
 before_run() {
   e
   e "Stop $(style bold)apache2"
   cmd_log "sudo service apache2 stop"
+  _fix_perm
 }
 
 after_run() {
   e
   e "Start $(style bold)apache2"
   cmd_log "sudo service apache2 start"
+  _fix_perm
 }
