c8975715a9db48f8f0278f5708296b5d0a90b155
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-11-12 18:06:50 -0300

    Home User

--- src/sources/docker-files/init.sh
+++ src/sources/docker-files/init.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Executed every run
 
-if [ ! -d "~/.ssh" ]; then
+export USER_HOME="/home"
+
+if [ ! -d "$USER_HOME/.ssh" ]; then
   if [ -d /root/home/.ssh ]; then
-    cp -r /root/home/.ssh ~/.ssh
-    chown -R $USERNAME:$USERNAME ~/.ssh
-    chmod -R 400 ~/.ssh
+    cp -r /root/home/.ssh $USER_HOME/.ssh
+    chown -R $USERNAME:$USERNAME $USER_HOME/.ssh
+    chmod -R 400 $USER_HOME/.ssh
   fi
 fi
 
-if [ ! -d "~/.git" ]; then
-  [ -d "/root/home/.git" ] && cp -r /root/home/.git ~/.git
+if [ ! -d "$USER_HOME/.git" ]; then
+  [ -d "/root/home/.git" ] && cp -r /root/home/.git $USER_HOME/.git
 fi
 
 service mysql start
@@ -21,9 +23,9 @@ if ! getent passwd $DOCKDEV_USER_NAME > /dev/null
     echo "Creating user $DOCKDEV_USER_NAME:$DOCKDEV_GROUP_NAME"
     groupadd --gid $DOCKDEV_GROUP_ID -r $DOCKDEV_GROUP_NAME
     useradd --system --uid=$DOCKDEV_USER_ID --gid=$DOCKDEV_GROUP_ID \
-      --home-dir /home --password $DOCKDEV_USER_NAME $DOCKDEV_USER_NAME
+      --home-dir $USER_HOME --password $DOCKDEV_USER_NAME $DOCKDEV_USER_NAME
     usermod -a -G sudo $DOCKDEV_USER_NAME
-    chown -R $DOCKDEV_USER_NAME:$DOCKDEV_GROUP_NAME /home
+    chown -R $DOCKDEV_USER_NAME:$DOCKDEV_GROUP_NAME $USER_HOME
   fi
 
 sudo -u $DOCKDEV_USER_NAME zsh
